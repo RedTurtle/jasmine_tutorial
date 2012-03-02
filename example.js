5aedbd1d2b3ea41a4915111b9dfca50ba9dc96ea
Yiorgis Gozadinos <ggozad@jarn.com>
2012-03-02 17:16:30 +0100

    Example of spies and module init

--- example.js
+++ example.js
@@ -1,6 +1,30 @@
 (function ($) {
-	window.adder = function (x, y) {
-		return x+y;
+	var rt = {
+
+		initialized: null,
+		uid: null,
+
+		initialize: function (url) {
+			url = url || '/getData';
+			rt.initialized = $.getJSON(url);
+			rt.initialized.done(function (json) {rt.uid = json.uid;});
+			return rt.initialized;
+		},
+
+		doStuff: function (data) {
+			var d = $.Deferred();
+
+			rt.initialized.done(function () {
+				data.uid = rt.uid;
+				d.resolve(data);
+			});
+
+			return d.promise();
+		}
+
 	};
+
+	//rt.initialize();
+	window.rt = rt;
 
 })(window.jQuery);
--- tests/specs/example_spec.js
+++ tests/specs/example_spec.js
@@ -1,17 +1,67 @@
-(function ($, Jasmine) {
+/*globals describe: false, it:false, expect:false, spyOn:false */
+(function ($, jasmine, rt) {
 
 	describe('Example module', function () {
 
-		it('defines an adder function on "window"', function () {
-			expect(window.adder).toBeDefined();
+		it('provides rt on window', function () {
+			expect(window.rt).toBeDefined();
 		});
 
-		it('adds x and y together', function () {
+		it('does not initialize rt if getJSON fails', function () {
+			var spy = spyOn($, 'getJSON').andCallFake(function (url) {
+				expect(url).toEqual('/getData');
+				var d = $.Deferred();
+				d.reject();
+				return d.promise();
+			});
 
-			expect(adder(34, 5)).toEqual(39);
+			var success = jasmine.createSpy(),
+				failure = jasmine.createSpy();
+			var p = rt.initialize();
+			p.done(success);
+			p.fail(failure);
+			expect(failure).wasCalled();
+			expect(success).wasNotCalled();
+		});
 
+		it('makes a JSON call on load to /getData', function () {
+			var spy = spyOn($, 'getJSON').andCallFake(function (url) {
+				expect(url).toEqual('/getData');
+				var d = $.Deferred();
+				d.resolve({uid: 'myuid'});
+				return d.promise();
+			});
+			var success = jasmine.createSpy(),
+				failure = jasmine.createSpy();
+			var p = rt.initialize();
+			p.done(success);
+			p.fail(failure);
+			expect(spy).wasCalled();
+			expect(success).wasCalled();
+			expect(failure).wasNotCalled();
+			expect(rt.uid).toEqual('myuid');
+		});
+
+		it('appends uid to data and return the data once rt has been initialized', function () {
+			var spy = spyOn($, 'getJSON').andCallFake(function (url) {
+				var d = $.Deferred();
+				d.resolve({uid: 'myuid'});
+				return d.promise();
+			});
+			rt.initialize();
+			var p = rt.doStuff({text: 'Hello world'}),
+				success = jasmine.createSpy(),
+				failure = jasmine.createSpy();
+			p.done(success);
+			p.fail(failure);
+			expect(success).wasCalled();
+			expect(failure).wasNotCalled();
+			p.done(function (data) {
+				expect(data.uid).toEqual('myuid');
+				expect(data.text).toEqual('Hello world');
+			});
 		});
 
 	});
 
-})(window.jQuery, window.Jasmine);
+})(window.jQuery, window.jasmine, window.rt);
